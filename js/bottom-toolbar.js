f5ab7a56e459deec2bedca1f5fc40d19143a199c
nwinters13 <nathan.winters@tufts.edu>
2015-02-27 17:21:43 -0500

    image modifications

--- js/bottom-toolbar.js
+++ js/bottom-toolbar.js
@@ -10,3 +10,16 @@ function resetButtons() {
 	document.getElementById('events-link').style.border = "none"	
 	document.getElementById('love-link').style.border = "none"	
 }
+
+function modifyDivs() {
+	console.log("modifying");
+	var launch = document.getElementById('launch');
+	var w = launch.offsetWidth;
+	var aspectRatio = 1024/681;
+	var newHeight = w / aspectRatio;
+	launch.style.height = newHeight;
+	console.log(w);
+	console.log(newHeight);
+	var h = newHeight + "px";
+	document.getElementById('launch').style.height = h;
+}
